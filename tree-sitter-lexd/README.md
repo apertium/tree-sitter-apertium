f48a2bf345eaa8e166465e50115d8078c8a069f5
Daniel Swanson <popcorn.tomato.dude@gmail.com>
2021-02-02 18:51:22 -0600

    More work on lexd
    (probably mostly done now, but will wait to see how highlighting goes)

---
+++ tree-sitter-lexd/README.md
@@ -0,0 +1,8 @@
+# tree-sitter-lexd
+
+A tree-sitter grammar of lexd.
+
+# Links
+
+https://github.com/apertium/lexd
+https://github.com/apertium/lexd/blob/master/Usage.md
--- tree-sitter-lexd/grammar.js
+++ tree-sitter-lexd/grammar.js
@@ -3,86 +3,175 @@ module.exports = grammar({
     
     extras: $ => [
         $.comment,
-        /\s/
     ],
 
     rules: {
-        source_file: $ => repeat(
-            choice($.pattern_block,
-                   $.lexicon_block,
-                   $.alias_command)
+        source_file: $ => seq(
+            optional($._nl),
+            repeat(choice(
+                $.pattern_block,
+                $.lexicon_block,
+                $.alias_command,
+            ))
         ),
         
-        pattern_block: $ => prec(2, seq(
+        pattern_block: $ => seq(
             choice(
                 $.pattern_start,
                 seq($.named_pattern_start,
+                    $._ws,
                     $.identifier)
             ),
-            repeat(choice($.pattern_line, $._empty_line))
-        )),
+            $._nl,
+            repeat1($.pattern_line)
+        ),
 
-        pattern_start: $ => "PATTERNS",
-        named_pattern_start: $ => "PATTERN",
-        lexicon_start: $ => "LEXICON",
+        pattern_start: $ => token(prec(1, "PATTERNS")),
+        named_pattern_start: $ => token(prec(1, "PATTERN")),
+        lexicon_start: $ => token(prec(1, "LEXICON")),
         alias: $ => "ALIAS",
         left_sieve: $ => "<",
         right_sieve: $ => ">",
         colon: $ => ":",
-        
+
         pattern_line: $ => seq(
-            repeat1($.pattern_token),
-            "\n"
+            $.pattern_token,
+            repeat(seq($._ws, $.pattern_token)),
+            $._nl
+        ),
+
+        _ident_number: $ => seq(
+            $.identifier,
+            optional($.number)
+        ),
+
+        pattern_operator: $ => choice(
+            "*",
+            "+",
+            "?"
+        ),
+
+        anonymous_pattern: $ => seq(
+            "(",
+            $.pattern_token,
+            repeat(seq($._ws, $.pattern_token)),
+            ")"
+        ),
+
+        _modifiable_pat_tok: $ => choice(
+            $._ident_number,
+            prec(2, seq($._ident_number, $.colon)),
+            seq($.colon, $._ident_number),
+            $.anonymous_lexicon,
+            $.anonymous_pattern
         ),
         
         pattern_token: $ => choice(
             $.left_sieve,
             $.right_sieve,
-            seq($.identifier,
-                optional($.number)
-               ),
-            $.anonymous_lexicon,
-            seq(
-                "(",
-                repeat($.pattern_token),
-                ")"
-            )
+            prec.left(2, seq(
+                $._modifiable_pat_tok,
+                optional($.tag_filter),
+                optional($.pattern_operator)
+            ))
+        ),
+
+        _tag_or_neg: $ => seq(
+            optional("-"),
+            $.tag
+        ),
+        
+        _tag_list: $ => seq(
+            $._tag_or_neg,
+            optional($._ws),
+            repeat(prec.left(2, seq(
+                ",",
+                optional($._ws),
+                $._tag_or_neg,
+                optional($._ws),
+            ))),
+        ),
+
+        tag_setting: $ => seq(
+            "[",
+            optional($._ws),
+            $._tag_list,
+            "]"
+        ),
+
+        tag_distribution_operator: $ => choice(
+            "^",
+            "|"
+        ),
+        
+        tag_distribution: $ => seq(
+            $.tag_distribution_operator,
+            "[",
+            optional($._ws),
+            $._tag_list,
+            "]"
+        ),
+
+        _tag_filter_segment: $ => choice(
+            $.tag_distribution,
+            $._tag_or_neg
+        ),
+        
+        tag_filter: $ => seq(
+            "[",
+            optional($._ws),
+            $._tag_filter_segment,
+            repeat(seq(
+                optional($._ws),
+                ",",
+                optional($._ws),
+                $._tag_filter_segment
+            )),
+            optional($._ws),
+            "]"
         ),
         
         anonymous_lexicon: $ => seq(
             "[",
+            optional($._ws),
             $.lexicon_segment,
+            optional($._ws),
             "]"
         ),
         
         lexicon_block: $ => prec(2, seq(
             $.lexicon_start,
+            $._ws,
             $.identifier,
             optional($.number),
-            "\n",
-            repeat(choice($.lexicon_line, $._empty_line))
+            optional($.tag_setting),
+            $._nl,
+            repeat1($.lexicon_line)
         )),
         
         lexicon_line: $ => seq(
             $.lexicon_segment,
             repeat(seq(
-                /\s/,
+                $._ws,
                 $.lexicon_segment
             )),
-            "\n"
+            $._nl
         ),
 
         _lexicon_side: $ => repeat1(
             choice($.lexicon_string, $.escaped_char)
         ),
 
-        lexicon_segment: $ => choice(
-            $._lexicon_side,
-            seq(
-                optional($._lexicon_side),
-                $.colon,
-                optional($._lexicon_side)
-            )
+        lexicon_segment: $ => seq(
+            choice(
+                $._lexicon_side,
+                seq(
+                    optional($._lexicon_side),
+                    $.colon,
+                    optional($._lexicon_side)
+                )
+            ),
+            optional($.tag_setting)
         ),
 
         lexicon_string: $ => /[^\s\n\\#:\[\]]+/,
@@ -93,15 +182,23 @@ module.exports = grammar({
 
         alias_command: $ => seq(
             alias($.alias, "ALIAS"),
+            $._ws,
             $.identifier,
+            $._ws,
             $.identifier,
-            "\n"
+            $._nl
         ),
 
         identifier: $ => /[^\s?|()<>\[\]*+]+/,
+        tag: $ => /[^\s?|()<>\[\]*+,^-][^\s?|()<>\[\]*+,^]*/,
 
         number: $ => /\(\d+\)/,
 
-        _empty_line: $ => "\n"
+        _nl: $ => seq(
+            optional($._ws),
+            "\n",
+            repeat(choice($._ws, "\n"))
+        ),
+        _ws: $ => /[ \t]+/
     }
 });
